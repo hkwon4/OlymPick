a23afc91df97bfa4f0555245f4aee1ae95c94fc6
nichanl <lamanichan01@gmail.com>
2024-02-12 21:20:19 -0800

    about

--- application/main.py
+++ application/main.py
@@ -10,7 +10,7 @@ people_info = {
     'Fadee Ghiragosian': {'name': 'Fadee Ghiragosian', 'role': 'Backend Lead', 'about': ' I am pursuing a degree in Computer Science, my family is from Egypt and Armenia, and I love playing video games. '},
     'Ethan Ho': {'name': 'Ethan Ho', 'role': 'Github Lead', 'about': ' Last semester for CS Degree. I build custom keyboards, play video games, and read in my spare time. '},
     'Abby Lin': {'name': 'Abby Lin', 'role': 'Database', 'about': ''},
-    'Nichan Lama': {'name': 'Nichan Lama', 'role': 'Backend', 'about': ''},
+    'Nichan Lama': {'name': 'Nichan lama', 'role': 'Backend', 'about': 'I am a CS major. I love trying out new food and working out.'},
     'Zabiullah Niemati': {'name': 'Zabiullah Niemati', 'role': 'Frontend', 'about': ''},
     'Zizo Ezzat': {'name': 'Zizo Ezzat', 'role': 'Frontend', 'about': '4th year CS Major, I love working out and listening to music.'},
 }
