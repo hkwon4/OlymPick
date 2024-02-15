388219f89bf413f16037448115ebf49eb46b63e9
Hyok In Kwon <99067273+hkwon4@users.noreply.github.com>
2024-02-14 17:17:31 -0800

    Update about me for Johnny and Zabi

--- application/main.py
+++ application/main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 # Change your name, role in the group, and put some details in about
 # To run, make sure your directory is in application, then type in the terminal "python main.py" (Flask required)
 people_info = {
-    'Johnny Kwon': {'name': 'Johnny Kwon', 'role': 'Team Lead', 'about': ''},
+    'Johnny Kwon': {'name': 'Johnny Kwon', 'role': 'Team Lead', 'about': 'I am the team leader of this project and a Senior CS Major of SFSU. Always learning and look forward to suffering in this field'},
     'Fadee Ghiragosian': {'name': 'Fadee Ghiragosian', 'role': 'Backend Lead', 'about': ' I am pursuing a degree in Computer Science, my family is from Egypt and Armenia, and I love playing video games. '},
     'Ethan Ho': {'name': 'Ethan Ho', 'role': 'Github Lead', 'about': ' Last semester for CS Degree. I build custom keyboards, play video games, and read in my spare time. '},
     'Abby Lin': {'name': 'Abby Lin', 'role': 'Database', 'about': ' Last Semester CS Major, an international transfer student from Taiwan. I love music, swimming, and playing games.'},
     'Nichan Lama': {'name': 'Nichan lama', 'role': 'Backend', 'about': 'I am a CS major. I love trying out new food and working out.'},
-    'Zabiullah Niemati': {'name': 'Zabiullah Niemati', 'role': 'Frontend', 'about': ''},
+    'Zabiullah Niemati': {'name': 'Zabiullah Niemati', 'role': 'Frontend', 'about': 'My name is Zabiullah Niemati, and hopefully, this will be my last semester at SFSU. I am majoring in computer science. I already hold a bachelor's degree in engineering from overseas, and this will be my second bachelor's degree.'},
     'Zizo Ezzat': {'name': 'Zizo Ezzat', 'role': 'Frontend', 'about': '4th year CS Major, I love working out and listening to music.'},
 }
 
